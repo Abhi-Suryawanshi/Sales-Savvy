c8fabe85ef43814f68060c09f8ad45148a7e07e1
Abhishek Suryawanshi <abhishekfs01@gmail.com>
2025-06-04 13:07:16 +0530

    Initial commit

---
+++ SALES-SAVVY-FRONTEND/sales-savvy/src/pages/Welcome.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Welcome() {
+  return (
+    <>
+        <h4>Welcome page</h4>
+        
+    </>
+  )
+}
---
+++ com.salesSavvy/src/main/java/com/salesSavvy/service/UsersServiceImplementation.java
@@ -0,0 +1,20 @@
+package com.salesSavvy.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.salesSavvy.entity.Users;
+import com.salesSavvy.repository.UsersRepository;
+
+@Service
+public class UsersServiceImplementation implements UsersService {
+	@Autowired
+	UsersRepository repo;
+
+	public void signUp(Users user) {
+		repo.save(user);
+	}
+	public Users getUser(String username) {
+		return repo.findByUsername(username);
+	}
+}
