ff288cee7b9a0cbbc4afd8b0195a64befa812afd
Abhishek Suryawanshi <abhishekfs01@gmail.com>
2025-06-10 10:23:34 +0530

    Add routing and form handling for user authentication

--- SalesSavvy-Frontend/sales-savvy/src/App.jsx
+++ SalesSavvy-Frontend/sales-savvy/src/App.jsx
@@ -1,15 +1,17 @@
+import { Route, Routes } from "react-router-dom";
 import Sign_in from './pages/Sign_in'
 import Sign_up from './pages/Sign_up'
+import Welcome from './pages/Welcome'
 
 function App() {
   
   return (
     <>
-      <h4>Welcome to Sales Savvy!</h4>
-
-      <Sign_up />
-      <hr></hr>
-      <Sign_in />
+      <Routes>
+          <Route path = "/" element = {<Welcome />} />
+          <Route path = "/sign_up" element = {<Sign_up />} />
+          <Route path = "/sign_in" element = {<Sign_in />} />
+      </Routes>
     </>
   )
 }
